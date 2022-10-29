7243d6b6193e23b9cdcef0f7e5029b0efcde8678
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-10-29 14:04:17 +0200

    dynamic routes for navbar and fix margin on arabic

--- common/routes.tsx
+++ common/routes.tsx
@@ -1,26 +1,53 @@
 import {
-    AttachMoney,
-    Calculate,
-    HelpOutline,
-    LocalShipping,
-    Logout,
-    Paid,
-    Person,
-    PinDrop,
-    ShoppingBag,
-    ShoppingBasket,
-  } from "@mui/icons-material";
-  
-  export const routes = {
-    HowItWorks: { label: "How it works", href: "/howitworks", translationKey: "howItWorks", icon: HelpOutline },
-    CalculateShipping: { label: "Calculate Shipping", href: "/calculateshipping", translationKey: "calculateShipping", icon: Calculate },
-    Packages: { label: "Packages", href: "/account/packages", translationKey: "account.packages", icon: ShoppingBag },
-    Addresses: { label: "Addresses", href: "/account/addresses", translationKey: "account.addresses", icon: PinDrop },
-    Cartons: { label: "Cartons", href: "/account/cartons", translationKey: "account.cartons", icon: ShoppingBasket },
-    DeliveryRequests: { label: "Delivery Requests", href: "/account/deliveries", translationKey: "account.deliveries", icon: LocalShipping },
-    Transactions: { label: "Transactions", href: "/account/transactions", translationKey: "account.transactions", icon: Paid },
-    Wallet: { label: "Wallet", href: "/account/wallet", translationKey: "account.wallet", icon: AttachMoney },
-    PersonalInformation: { label: "Personal Information", href: "/account/personal", translationKey: "account.personal", icon: Person },
-    Logout: { label: "Logout", href: "/logout", translationKey: "account.logout", icon: Logout },
-  };
- 
+  AttachMoney,
+  Calculate,
+  HelpOutline,
+  LocalShipping,
+  Logout,
+  Paid,
+  Person,
+  PinDrop,
+  ShoppingBag,
+  ShoppingBasket,
+} from "@mui/icons-material";
+
+export const routes = {
+  HowItWorks: { label: "How it works", href: "/howitworks", translationKey: "howItWorks", icon: HelpOutline },
+  CalculateShipping: {
+    label: "Calculate Shipping",
+    href: "/calculateshipping",
+    translationKey: "calculateShipping",
+    icon: Calculate,
+  },
+  Packages: { label: "Packages", href: "/account/packages", translationKey: "account.packages", icon: ShoppingBag },
+  Addresses: { label: "Addresses", href: "/account/addresses", translationKey: "account.addresses", icon: PinDrop },
+  Cartons: { label: "Cartons", href: "/account/cartons", translationKey: "account.cartons", icon: ShoppingBasket },
+  DeliveryRequests: {
+    label: "Delivery Requests",
+    href: "/account/deliveries",
+    translationKey: "account.deliveries",
+    icon: LocalShipping,
+  },
+  Transactions: {
+    label: "Transactions",
+    href: "/account/transactions",
+    translationKey: "account.transactions",
+    icon: Paid,
+  },
+  Wallet: { label: "Wallet", href: "/account/wallet", translationKey: "account.wallet", icon: AttachMoney },
+  PersonalInformation: {
+    label: "Personal Information",
+    href: "/account/personal",
+    translationKey: "account.personal",
+    icon: Person,
+  },
+  Logout: { label: "Logout", href: "/logout", translationKey: "account.logout", icon: Logout },
+};
+
+export const navBarRoutes = [
+  { label: "Home", href: "/", translationKey: "routes.home" },
+  { label: "OurStory", href: "/ourstory", translationKey: "routes.ourStory" },
+  { label: "Services", href: "/services", translationKey: "routes.service" },
+  { label: "HowItWorks", href: "/howitworks", translationKey: "routes.howItWorks" },
+  { label: "Contact", href: "/contact", translationKey: "routes.contact" },
+];
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -11,19 +11,16 @@ import {
   ListItemText,
   Toolbar,
   Typography,
-  Button,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Container } from "@mui/system";
 import NavBarButton from "../fragments/NavBarButton";
 import Link from "next/link";
 import Image from "next/image";
-import { GetStaticProps } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import styles from "./NavBar.module.scss";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-
+import { navBarRoutes } from "../../common/routes";
 interface Props {
   /**
    * Injected by the documentation to work in an iframe.
@@ -72,44 +69,55 @@ const NavBar = (props: Props) => {
               aria-label="open drawer"
               edge="start"
               onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { md: "none" } }}
+              sx={{ display: { md: "none" }, margin: "0 5px" }}
             >
               <MenuIcon />
             </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
+            <Box
               sx={{
-                fontFamily: "Ubuntu",
+                cursor: "pointer",
                 fontWeight: "700",
+                margin: "0 15px",
                 flexGrow: 1,
-                display: { xs: "block", sm: "block", color: "orange" },
+                display: { xs: "block", sm: "block", color: "#ff9a28" },
               }}
             >
               <Box sx={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
-                <Image src="/images/logo.png" width="40" height="40" alt="" />
+                <Image src="/images/logo.png" width="27" height="27" alt="" />
                 <Link href="/">
-                  <Typography sx={{ fontWeight: 600, fontSize: "min(3vw,16px)", cursor: "pointer" }}>
+                  <Typography
+                    sx={{ fontFamily: "Ubuntu", fontWeight: 600, fontSize: "min(3vw,16px)", margin: "0 5px" }}
+                  >
                     MY STORAGE
                   </Typography>
                 </Link>
               </Box>
-            </Typography>
+            </Box>
             <Box sx={{ display: { xs: "none", md: "flex" }, marginRight: 3 }}>
-              {navItems.map((item, index) => (
-                <NavBarButton key={index} title={t(`${item}`)}></NavBarButton>
+              {navBarRoutes.map((item) => (
+                <Link href={item.href} key={item.label}>
+                  <a>
+                    <NavBarButton title={t(`${item.translationKey}`)}></NavBarButton>
+                  </a>
+                </Link>
               ))}
             </Box>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <Link href="/login">
                 <AccountBoxIcon
-                  sx={{ color: "#393939", margin: "0 10px", fontSize: "30px", display: { sm: "none" } }}
+                  sx={{
+                    color: "#393939",
+                    margin: "0 10px",
+                    fontSize: "30px",
+                    display: { sm: "none" },
+                    cursor: "pointer",
+                  }}
                 />
               </Link>
               {["routes.login", "routes.calculate"].map((item, index) => (
                 <Link href="/login" key={index}>
                   <a>
-                    <NavBarButton title={t(`${item}`)}></NavBarButton>
+                    <NavBarButton key={item} title={t(`${item}`)}></NavBarButton>
                   </a>
                 </Link>
               ))}
